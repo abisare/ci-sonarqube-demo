783e26b7bf5f5b2679cbda2fdc542dffdbf41761
Oleksii Martyniuk <o.martyniuk@prozorro.ua>
2019-11-13 23:24:28 +0200

    Initial commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#install wget and unzip if not present!
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
+unzip sonar-scanner-cli-4.0.0.1744-linux.zip -d ./sonar-scanner/
+
