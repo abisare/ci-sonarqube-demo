5bf391c4fb2bb1724eb29b7862692936d33ff7cb
Oleksii Martyniuk <o.martyniuk@prozorro.ua>
2019-11-13 23:34:03 +0200

    Fixes

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 #install wget and unzip if not present!
 
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
-unzip sonar-scanner-cli-4.0.0.1744-linux.zip -d ./sonar-scanner/
+unzip sonar-scanner-cli-4.0.0.1744-linux.zip
 
